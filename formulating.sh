1de458a72e5309138df6eaea37752aed5ff9adad
0hana <0hana@users.noreply.github.com>
2022-06-08 15:32:48 -0700

    Defines in formulating.sh: code_object_info structure

--- formulating.sh
+++ formulating.sh
@@ -48,8 +48,31 @@ test_names="$(basename -a ${2} | grep '^\$hanamake_test\$')"
 for name in ${test_names}
 do
 >>${1} echo "void ${name}"
->>${1} echo '( FILE      ** const $hanamake_test$log_file'
->>${1} echo ', char const * const $hanamake_test$log_path'
->>${1} echo ') ;'
+>>${1} echo "( FILE      ** const \$hanamake_test\$log_file"
+>>${1} echo ", char const * const \$hanamake_test\$log_path"
+>>${1} echo ") ;"
 >>${1} echo
 done
+
+# Get number of code objects
+>>${1} echo "#define code_objects $(echo ${2} | wc -w)"
+
+# Define code object information structure for access in dispatcher loop
+>>${1} echo \
+"\
+struct code_object_info
+{
+  char   const * const name;
+  /*----------------------*/
+  char   const * const log_path;
+  FILE         *       log_file;
+  /*--------------------------*/
+  size_t const * const dependency;
+  size_t const         dependencies;
+  /*------------------------------*/
+  void        (* const \$hanamake_test\$)
+  ( FILE      ** const \$hanamake_test\$log_file
+  , char const * const \$hanamake_test\$log_path
+  ) ;
+} ;
+"
