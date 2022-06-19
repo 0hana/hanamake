c485a971499ee46be2b26a7890d080c01f62df4d
0hana <0hana@users.noreply.github.com>
2022-06-18 20:46:40 -0700

    Changes hanamake generated data tag from 'hanamake' -> 'hanamade'
    The idea is that hanamake is the command, the action.
    hanamade is what was made, the consequence of action.

--- hanamake-components/formulating.sh
+++ hanamake-components/formulating.sh
@@ -44,12 +44,12 @@
 "
 
 # Insert unit test declarations
-test_names="$(basename -a ${2} | grep '^__hanamake_test__')"
+test_names="$(basename -a ${2} | grep '^__hanamade_test__')"
 for name in ${test_names}
 do
 >>${1} echo "void ${name}"
->>${1} echo "( FILE      ** const __hanamake_test__log_file"
->>${1} echo ", char const * const __hanamake_test__log_path"
+>>${1} echo "( FILE      ** const __hanamade_test__log_file"
+>>${1} echo ", char const * const __hanamade_test__log_path"
 >>${1} echo ") ;"
 >>${1} echo
 done
@@ -91,9 +91,9 @@ struct code_object_info
   size_t const * const dependency;
   size_t const         dependencies;
   /*------------------------------*/
-  void        (* const __hanamake_test__)
-  ( FILE      ** const __hanamake_test__log_file
-  , char const * const __hanamake_test__log_path
+  void        (* const __hanamade_test__)
+  ( FILE      ** const __hanamade_test__log_file
+  , char const * const __hanamade_test__log_path
   ) ;
   /*------------------------------------------*/
   enum code_object_status status;
@@ -104,14 +104,14 @@ coi[code_objects] =
 "
 
 # Identify longest code object name for test output formatting (relative right justify names)
-hanamake_test_prefix_length=$(printf '__hanamake_test__' | wc -m)
+hanamade_test_prefix_length=$(printf '__hanamade_test__' | wc -m)
 translation_length=8
 longest_base=0
 for name in $(basename -a ${2})
 do
 	name_length=$(printf ${name} | wc -m)
-	if test -n "$(echo   ${name} | grep '^__hanamake_test__')"
-	then name_length=$((${name_length} - ${hanamake_test_prefix_length} + ${translation_length}))
+	if test -n "$(echo   ${name} | grep '^__hanamade_test__')"
+	then name_length=$((${name_length} - ${hanamade_test_prefix_length} + ${translation_length}))
 	fi
 	if test ${name_length} -gt ${longest_base}
 	then longest_base=${name_length}
@@ -124,10 +124,10 @@ do
 	base=$(basename ${identity})
 	base_length=$(printf ${base} | wc -m)
 	# name string
-	if test -n "$(echo ${base} | grep '^__hanamake_test__')"
+	if test -n "$(echo ${base} | grep '^__hanamade_test__')"
 	then
-	base_length=$((${base_length} - ${hanamake_test_prefix_length} + ${translation_length}))
-	>>${1} echo "  { \"$(for i in $(seq $((${longest_base} - ${base_length}))); do printf ' '; done)$(echo "${base}" | sed 's/^__hanamake_test__//')\""
+	base_length=$((${base_length} - ${hanamade_test_prefix_length} + ${translation_length}))
+	>>${1} echo "  { \"$(for i in $(seq $((${longest_base} - ${base_length}))); do printf ' '; done)$(echo "${base}" | sed 's/^__hanamade_test__//')\""
 	else
 	>>${1} echo "  { \"$(for i in $(seq $((${longest_base} - ${base_length}))); do printf ' '; done)${base}\""
 	fi
@@ -137,11 +137,11 @@ do
 	>>${1} echo "  , NULL"                 # dependency
 	>>${1} echo "  , 0"                    # dependencies
 
-	if test -n "$(echo ${base} | grep '^__hanamake_test__')"
+	if test -n "$(echo ${base} | grep '^__hanamade_test__')"
 	then
-	>>${1} echo "  , ${base}"              # __hanamake_test__ function pointer
+	>>${1} echo "  , ${base}"              # __hanamade_test__ function pointer
 	else
-	>>${1} echo "  , NULL"                 # __hanamake_test__ function NULL pointer (not a test)
+	>>${1} echo "  , NULL"                 # __hanamade_test__ function NULL pointer (not a test)
 	fi
 
 	>>${1} echo "  , failed"               # code object status (default: failed)
@@ -206,14 +206,14 @@ int main(void)
 		switch(coi[index].status)
 		{
 			case updated:
-				printf(\"  %s%s  [ updated ]\n\", (coi[index].__hanamake_test__ ? \"(test)  \" : \"\"), coi[index].name);
+				printf(\"  %s%s  [ updated ]\n\", (coi[index].__hanamade_test__ ? \"(test)  \" : \"\"), coi[index].name);
 				updates++;
 
-				if(coi[index].__hanamake_test__) { tests_required_to_run++; }
+				if(coi[index].__hanamade_test__) { tests_required_to_run++; }
 				break;
 
 			case depends:
-				printf(\"  %s%s  [ depends on: \", (coi[index].__hanamake_test__ ? \"(test)  \" : \"\"), coi[index].name);
+				printf(\"  %s%s  [ depends on: \", (coi[index].__hanamade_test__ ? \"(test)  \" : \"\"), coi[index].name);
 
 				printf(\"%s\", coi[coi[index].dependency[0]].name);
 
@@ -222,13 +222,13 @@ int main(void)
 
 				printf(\" ]\n\");
 
-				if(coi[index].__hanamake_test__) tests_required_to_run++;
+				if(coi[index].__hanamade_test__) tests_required_to_run++;
 				break;
 
 			case  failed:
-				printf(\"  %s%s  [ failing ]\n\", (coi[index].__hanamake_test__ ? \"(test)  \" : \"\"), coi[index].name);
+				printf(\"  %s%s  [ failing ]\n\", (coi[index].__hanamade_test__ ? \"(test)  \" : \"\"), coi[index].name);
 
-				if(coi[index].__hanamake_test__) tests_required_to_run++;
+				if(coi[index].__hanamade_test__) tests_required_to_run++;
 				break;
 
 			case  passed: /* do nothing */
@@ -249,9 +249,9 @@ int main(void)
 
 	for(size_t index = 0; index < code_objects; index++)
 	{
-		if(coi[index].__hanamake_test__ != NULL && coi[index].status != passed)
+		if(coi[index].__hanamade_test__ != NULL && coi[index].status != passed)
 		{
-			coi[index].__hanamake_test__(&(coi[index].log_file), coi[index].log_path);
+			coi[index].__hanamade_test__(&(coi[index].log_file), coi[index].log_path);
 
 			if(coi[index].log_file != NULL)
 			{
