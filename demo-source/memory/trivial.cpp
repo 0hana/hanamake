6b5f1541de6d7291465b3479181b72e48f64e515
0hana <0hana@users.noreply.github.com>
2022-11-06 20:09:44 -0800

    Achieves C++ compatability -- All that remains is full formulating.sh integration and 'debug' mode

--- demo-source/memory/trivial.cpp
+++ demo-source/memory/trivial.cpp
@@ -1,3 +1,14 @@
 namespace NS_test {
 	int functionA(int X, char Y) { return X + Y; }
 }
+
+
+hanamake_test(NS_test::functionA(int, char))
+{
+  hanamake_assert(NS_test::functionA(10, 'A') == 75);
+  hanamake_assert(NS_test::functionA(10, 'B') == 76);
+  hanamake_assert(NS_test::functionA(10, 'C') == 77);
+  hanamake_assert(NS_test::functionA(10, 'D') == 78);
+  hanamake_assert(NS_test::functionA(10, 'E') == 79);
+  hanamake_assert(NS_test::functionA(10, 'F') == 80);
+}
